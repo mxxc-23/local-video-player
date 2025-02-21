4707e38a5cf43d43f52d425d0609d73d38a1c019
mxxc-23 <cyprushervey23@gmail.com>
2025-02-21 13:54:23 +0800

    frontend

---
+++ public/script.js
@@ -0,0 +1,48 @@
+const fileInput = document.getElementById('fileInput');
+const videoPlayer = document.getElementById('videoPlayer');
+const volumeControl = document.getElementById('volume');
+const playlist = document.getElementById('playlist');
+let fileQueue = [];
+
+fileInput.addEventListener('change', function(event) {
+    fileQueue = Array.from(event.target.files);
+    playlist.innerHTML = '';
+    
+    fileQueue.forEach((file, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = file.name;
+        listItem.addEventListener('click', () => playVideo(index));
+        playlist.appendChild(listItem);
+    })
+
+    if (fileQueue.length > 0) {
+        playVideo(0);
+    }
+});
+
+function playVideo(index) {
+    if (fileQueue[index]) {
+        const objectURL = URL.createObjectURL(fileQueue[index]);
+        videoPlayer.src = objectURL;
+        videoPlayer.load();
+        videoPlayer.play();
+    }
+}
+
+// Volume control with gain boost
+volumeControl.addEventListener('input', function(event) {
+    videoPlayer.volume = Math.min(event.target.value, 1);
+    const gainValue = event.target.value;
+
+    if(!videoPlayer.audioContext) {
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaElementSource(videoPlayer);
+        const gainNode = audioContext.createGain();
+        gainNode.gain.value = gainValue;
+        source.connect(gainNode);
+        videoPlayer.audioContext = audioContext;
+        videoPlayer.gainNode = gainNode;
+    } else {
+        videoPlayer.gainNode.gain.value = gainValue;
+    }
+});
